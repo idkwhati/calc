ae6c1b24bc5b7ca2193cf487faffe29f42dc7e6e
idkwhati <wieczor4@op.pl>
2018-08-14 23:10:25 +0200

    Added type in equation JS script, improved smth

--- src/Calc.java
+++ src/Calc.java
@@ -1,47 +1,61 @@
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import java.util.Scanner;
 
 
 public class Calc {
     static Scanner s = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        //??? how to decalre as global variable????
+    public static void main(String[] args) throws ScriptException {
+
 
         while (true) {
-            System.out.println("What type of calculation?\n 1.Float\n 2.Int");
-            int firstSelect = s.nextInt();
-            if (firstSelect == 1) {//calculation on Float
-                System.out.println("What kind of operation would you like to use?\n 1.Addition\n 2.Subtraction\n 3.Multiplication\n 4.Division");
+            System.out.println("Choose type of calculator?\n 1.Type in equation\n 2.Choose from list");
 
-                int option = s.nextInt();
-                if (option > 4 || option == 0 || option < 0) {
-                    System.out.println("Error, wrong operation!");
+            int typeOfCalculator = s.nextInt();
+            if (typeOfCalculator == 1) {
+
+                System.out.println("Type in equation (e.g. 5+5)");
+                String equation= s.next();
+                ScriptEngineManager manager = new ScriptEngineManager();
+                ScriptEngine se = manager.getEngineByName("JavaScript");
+                Object result = se.eval(equation);
+                System.out.println(result);
+                askingForEndingProgram();
+            } else if (typeOfCalculator == 2) {
+
+                System.out.println("What type of calculation?\n 1.Float\n 2.Int");
+                int typeOfCalculationSelect = s.nextInt();
+                if (typeOfCalculationSelect == 1) {//calculation on Float
+                    System.out.println("What kind of operation would you like to use?\n 1.Addition\n 2.Subtraction\n 3.Multiplication\n 4.Division");
+
+                    int optionKindOfOperation = s.nextInt();
+                    if (optionKindOfOperation > 4 || optionKindOfOperation <= 0) {
+                        System.out.println("Error, wrong operation!");
+                    } else {
+                        System.out.println("Type in numbers: ");
+                        float no1 = s.nextFloat();
+                        float no2 = s.nextFloat();
+                        calculateOnFloat(no1, no2, optionKindOfOperation);
+                        askingForEndingProgram();
+                    }
+                } else if (typeOfCalculationSelect == 2) {//calcuation on Int
+                    System.out.println("What kind of operation would you like to use?\n 1.Addition\n 2.Subtraction\n 3.Multiplication\n 4.Division");
+
+                    int optionKindOfOperation = s.nextInt();
+                    if (optionKindOfOperation > 4 || optionKindOfOperation <= 0) {
+                        System.out.println("Error, wrong operation!");
+                    } else {
+                        System.out.println("Type in numbers: ");
+                        int no1 = s.nextInt();
+                        int no2 = s.nextInt();
+                        calculateOnInt(no1, no2, optionKindOfOperation);
+                        askingForEndingProgram();
+                    }
                 } else {
-                    System.out.println("Type in numbers: ");
-                    float no1 = s.nextFloat();
-                    float no2 = s.nextFloat();
-                    calculateOnFloat(no1, no2, option);
-
-                    endOfProgram();
+                    System.out.println("Wrong argument!");
                 }
-            } else if (firstSelect == 2) {//calcuation on Int
-                System.out.println("What kind of operation would you like to use?\n 1.Addition\n 2.Subtraction\n 3.Multiplication\n 4.Division");
-
-
-                int option = s.nextInt();
-                if (option > 4 || option == 0 || option < 0) {
-                    System.out.println("Error, wrong operation!");
-                } else {
-                    System.out.println("Type in numbers: ");
-                    int no1 = s.nextInt();
-                    int no2 = s.nextInt();
-                    calculateOnInt(no1, no2, option);
-                    endOfProgram();
-
-                }
-
-            } else {
-                System.out.println("Wrong argument!");
             }
         }
     }
@@ -102,12 +116,12 @@ public class Calc {
 
     }
 
-    private static void endOfProgram() {
+    private static void askingForEndingProgram() {
         while (true) {
             System.out.println("Do you want to recalc? (y-yes | n-no) ");
             char lastOption = s.next(".").charAt(0);
             if (lastOption == 'y') {
-                break; //???
+                break;
             } else if (lastOption == 'n') {
                 System.exit(0);
             } else {
